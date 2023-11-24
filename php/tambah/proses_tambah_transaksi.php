63adee986481f0c64c5686fbbedd0342b96f0125
YogaCode21 <yogacode2006@gmail.com>
2023-11-24 08:44:26 +0800

    delete some unnecessary file

--- php/tambah/proses_tambah_transaksi.php
+++ php/tambah/proses_tambah_transaksi.php
@@ -1,5 +1,13 @@
 <?php 
-echo $_GET['kategoripelanggan'];
-echo $_POST['tanggal'];
-echo $_POST['nama-pelanggan'];
+ $kategori = $_GET['kategoripelanggan'];
+ $tanggal = $_POST['tanggal'];
+ $nama_pelanggan = $_POST['nama-pelanggan'];
+ $id_karyawan = $_SESSION['idkaryawan'];
+
+$id_pel = mysqli_fetch_assoc(
+    mysqli_query($koneksi,"SELECT idpelanggan FROM tb_pelanggan WHERE namalengkap='$nama_pelanggan'")
+);
+
+$id_pelanggan = $id_pel['idpelanggan'];
+mysqli_query($koneksi,"INSERT INTO tb_transaksi VALUES(NULL,'$id_pelanggan','$id_karyawan','$tanggal','$kategori',0,0,0)");
 ?>
